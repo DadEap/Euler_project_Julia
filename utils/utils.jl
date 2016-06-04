301fc3da7e5d5aabffb228bb3f9504bef698fd4c
DadEap <boas.dav@gmail.com>
2016-06-04 22:08:12 +0200

    Quickfixes

--- utils/utils.jl
+++ utils/utils.jl
@@ -1,12 +1,12 @@
 function fibonacci(n::Int64)
-    fib_values = [1, 1]
+    fib_values = [0, 1]
     if (n < 0) return -1 end
-    if (n == 1 || n == 0) return 1 end
-    i = 3
+    if (n == 1) return 1 end
+    i = 2
     while i <= n
-        fib = fib_values[i-1] + fib_values[i-2]
+        fib = fib_values[i] + fib_values[i-1]
         push!(fib_values, fib)
         i = i + 1
     end
-    return fib_values[n]
+    return fib_values[n + 1]
 end
